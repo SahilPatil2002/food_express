b0e31723013ebe0682d9d0844193d580212d82c9
Sahil Patil <sahilpatil1616@gmail.com>
2025-06-04 13:40:44 +0530

    Search Screen UI and Navigation

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:food_express/screens/product_detail_screen.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:food_express/controllers/search_controller.dart';
@@ -50,7 +53,7 @@ class _SearchScreenState extends State<SearchScreen>
                 children: [
                   GestureDetector(
                     onTap: () {
-                      searchTextController.clear(); 
+                      searchTextController.clear();
                       Get.find<MySearchController>().clearSearchResults();
                       Get.back();
                     },
@@ -83,14 +86,14 @@ class _SearchScreenState extends State<SearchScreen>
                 child: Container(
                   height: 55,
                   decoration: BoxDecoration(
-                    border: Border.all(color: yellow, width: 1.5),
+                    // border: Border.all(color: yellow, width: 1.5),
                     // color: Colors.white,
                     borderRadius: BorderRadius.circular(16),
                   ),
                   padding: EdgeInsets.symmetric(horizontal: 16),
                   child: Row(
                     children: [
-                      Icon(Icons.restaurant, color: yellow),
+                      // Icon(Icons.restaurant, color: yellow),
                       SizedBox(width: 10),
                       Expanded(
                         child: TextField(
@@ -99,6 +102,10 @@ class _SearchScreenState extends State<SearchScreen>
                           onSubmitted: controller.searchMeals,
                           decoration: InputDecoration(
                             hintText: "Search for delicious meals...",
+                            hintStyle: GoogleFonts.poppins(
+                              color: Colors.grey,
+                              fontSize: 14,
+                            ),
                             border: InputBorder.none,
                           ),
                         ),
@@ -107,11 +114,7 @@ class _SearchScreenState extends State<SearchScreen>
                         onTap: () {
                           controller.searchMeals(textController.text);
                         },
-                        child: Icon(
-                          Icons.search,
-                          // size: 16,
-                          color: yellow,
-                        ),
+                        child: Icon(Icons.search, size: 30, color: yellow),
                       ),
                     ],
                   ),
@@ -119,7 +122,6 @@ class _SearchScreenState extends State<SearchScreen>
               ),
             ),
 
-            // 🔁 Result Area
             SizedBox(height: 20),
             Expanded(
               child: Obx(() {
@@ -149,73 +151,139 @@ class _SearchScreenState extends State<SearchScreen>
                   itemCount: controller.searchResults.length,
                   itemBuilder: (_, index) {
                     final meal = controller.searchResults[index];
-                    return Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      elevation: 2,
-                      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                      child: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Color.fromRGBO(250, 194, 45, 1),
-                                  width: 2,
-                                ),
-                                image: DecorationImage(
-                                  image: NetworkImage(meal.strMealThumb ?? ''),
-                                  fit: BoxFit.cover,
+                    final imageUrl = meal.strMealThumb ?? '';
+                    return InkWell(
+                      onTap: (){
+                        Get.to(() => ProductDetailScreen(meal: meal));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: NetworkImage(imageUrl),
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(
+                               Color.fromRGBO(0, 0, 0, 0.3),
+                              BlendMode.darken,
+                            ),
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                                child: Container(
+                                  color:  Color.fromRGBO(0, 0, 0, 0.7),
                                 ),
                               ),
-                            ),
-                            SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    meal.strMeal ?? 'Unknown Meal',
-                                    style: GoogleFonts.poppins(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 16,
-                                      color: yellow
-                                    ),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  SizedBox(height: 4),
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.location_on,
-                                        size: 15,
-                                        color: Colors.grey,
-                                      ),
-                                      SizedBox(width: 4),
-                                      Text(
-                                        meal.strArea ?? 'Unknown',
-                                        style: GoogleFonts.poppins(
-                                          fontSize: 15,
-                                          color: Colors.black87,
+                              Padding(
+                                padding: const EdgeInsets.all(14),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 60,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Color.fromRGBO(250, 194, 45, 1),
+                                          width: 2,
+                                        ),
+                                        image: DecorationImage(
+                                          image: NetworkImage(imageUrl),
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                ],
+                                    ),
+                                    SizedBox(width: 14),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            meal.strMeal ?? 'Unknown Meal',
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600,
+                                              color: Colors.white,
+                                            ),
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          SizedBox(height: 4),
+                                          Row(
+                                            children: [
+                                              Text(
+                                                meal.strArea ?? 'Unknown Area',
+                                                style: GoogleFonts.poppins(
+                                                  fontSize: 13,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                              SizedBox(width: 10),
+                                              Container(
+                                                padding: EdgeInsets.symmetric(
+                                                  horizontal: 8,
+                                                  vertical: 4,
+                                                ),
+                                                decoration: BoxDecoration(
+                                                  color: Color.fromRGBO(255, 244, 206, 1),
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                ),
+                                                child: Row(
+                                                  mainAxisSize: MainAxisSize.min,
+                                                  children: [
+                                                    Icon(
+                                                      Icons.fastfood,
+                                                      size: 12,
+                                                      color: Color.fromRGBO(
+                                                        250,
+                                                        194,
+                                                        45,
+                                                        1,
+                                                      ),
+                                                    ),
+                                                    SizedBox(width: 4),
+                                                    Text(
+                                                      meal.strCategory ??
+                                                          'Category',
+                                                      style: GoogleFonts.poppins(
+                                                        fontSize: 11,
+                                                        color: Colors.black,
+                                                        fontWeight: FontWeight.w500,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                      
+                                    Icon(
+                                      Icons.arrow_forward_ios_rounded,
+                                      size: 16,
+                                      color: Color.fromRGBO(250, 194, 45, 1),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_ios_rounded,
-                              size: 16,
-                              color: yellow,
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     );
